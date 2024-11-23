12b9b29530dd5dd7c3d1df584793149e9febafba
jblomquist2 <159498283+jblomquist2@users.noreply.github.com>
2024-11-23 20:16:49 +0000

    Updated Code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-AllowedVehiclesList = ["Ford F-150", "Chevrolet Silverado", "Tesla Cybertruck", "Toyota Tundra", "Nissan Titan"]
+AllowedVehiclesList = ["Ford F-150", "Chevrolet Silverado", "Tesla Cybertruck", "Toyota Tundra","Nissan Titan"]
 
 print("********************************")
 print("AutoCountry Vehicle Finder v0.3")
@@ -12,26 +12,26 @@ print("4. Exit")
 print("********************************")
 
 while True:
-  number = input(int)
-  if number == "1":
+  number = int(input())
+  if number == 1:
     print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
     for truck in AllowedVehiclesList:
       print(truck)
 
-  elif number == "2":
+  elif number == 2:
     response = input("Please enter the full Vehicle name: ")
     if response in AllowedVehiclesList:
       print(response + " is an authorized vehicle")
     else:
       print(response + " is not an authorized vehicle, if you received this in error please check the spelling and try again")
 
-  elif number == "3":
+  elif number == 3:
     with open("AllowedVehiclesList.txt", "a") as db:
       response = input("Please enter the full Vehicle name you would like to add: ")
       AllowedVehiclesList.append(response)
       db.write(response + "\n")
-      print("You have added " + response + " as an authorized vehicle" )
+      print('You have added "' + response + '" as an authorized vehicle')
 
-  elif number == "4":
+  elif number == 4:
     print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
     break
